fd8cfa7b879888dbeb9963baf1e6cad68dd66574
Kenny <kennethjlroos@gmail.com>
2020-06-07 14:13:19 +0200

    corrected config dict

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 iris_config = {'location': 'application/data/iris_anomalies.csv',
-               'features': ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'],
-               'target': 'Variety'}
+               'features': ['is_anomaly', 'petal.length', 'petal.width','sepal.length'],
+               'target': 'variety'}
