662e12d1ecb0784f21499af2ce8e6fdf39f5fde6
alrod97 <60501082+alrod97@users.noreply.github.com>
2020-06-08 14:35:25 +0200

    Update eva_main.py
    LLE implemented, 3D Scatter plots

--- alberto_outlier_detection/eva_main.py
+++ alberto_outlier_detection/eva_main.py
@@ -1,16 +1,16 @@
-import os
 import numpy as np
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_daq as daq
+import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import plotly.express as px
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial.distance import cdist  # fast distance matrices
 import pandas as pd
 from dash.dependencies import Input, Output
-
+from sklearn.manifold import LocallyLinearEmbedding
 
 class EvaData():
     def __init__(self):
@@ -29,12 +29,11 @@ class EvaData():
         self.outl_scores = None
         self.remained_variance = None
 
+
     def load_data(self, file_name):
-        '''
-        Loads a csv file into a numpy data X and labels y
+        ''' Loads a csv file into a numpy data X and labels y
         Definition:  load_data(self, file_name)
         Input:       fname   - string, file name ending in .csv
-
         '''
         self.file_name = file_name
         # Check data frame is in the correct .csv format
@@ -64,8 +63,7 @@ class EvaData():
             print('Error No CSV FILE') # here maybe some dash error widget-message
 
     def visualize_original_data(self):
-        '''
-        If the data is 2Dim or 3Dim, it displays the data in a 2D/3D Plot
+        ''' If the data is 2Dim or 3Dim, it displays the data in a 2D/3D Plot
         Definition:  visualize_original_data(self)
         '''
         if self.d == 1:
@@ -107,16 +105,14 @@ class EvaData():
         if self.d == 1:
             fig = px.scatter(x=self.X[:, 0], y=np.zeros(self.n), size=self.outl_scores, title='K-Ball-Index-Method for Outlier scores')
             return fig
-        if self.d == 2:
-            fig = px.scatter(x=self.X[:, 0], y=self.X[:, 1], size=self.outl_scores, title='K-Ball-Index-Method for Outlier scores')
-            return fig
-        if self.d == 3:
+
+        elif self.d == 3:
             fig = px.scatter_3d(x=self.X[:, 0], y=self.X[:, 1], z=self.X[:, 2], size=self.outl_scores, title='K-Ball-Index-Method for Outlier scores')
             return fig
         else:
-            fig = px.scatter(x=self.X[:, 0], y=self.X[:, 1], size=self.outl_scores,
-                             title='K-Ball-Index-Method for Outlier scores')
+            fig = px.scatter(x=self.X[:, 0], y=self.X[:, 1], size=self.outl_scores, title='K-Ball-Index-Method for Outlier scores')
             return fig
+
 
     def gamma_index(self, k, scaling_factor):
         ''' Computes Gamma index for the Data matrix K by computing the distances to its k nearest neighbours
@@ -146,21 +142,19 @@ class EvaData():
         if self.d == 1:
             fig = px.scatter(x=self.X[:, 0], y=np.zeros(self.n), size=self.outl_scores, title='Gamma-Index-Method for Outlier scores')
             return fig
-        if self.d == 2:
-            fig = px.scatter(x=self.X[:, 0], y=self.X[:, 1], size=self.outl_scores, title='Gamma-Index-Method for Outlier scores')
-            return fig
-        if self.d == 3:
+
+        elif self.d == 3:
             fig = px.scatter_3d(x=self.X[:, 0], y=self.X[:, 1], z=self.X[:, 2], size=self.outl_scores, title='Gamma-Index-Method for Outlier scores')
             return fig
         else:
             fig = px.scatter(x=self.X[:, 0], y=self.X[:, 1], size=self.outl_scores, title='Gamma-Index-Method for Outlier scores')
             return fig
 
+
     def visualize_outliers(self, threshold):
         '''
         Visualises the outliers of a dataset depending on the method that has been used to compute the outlier scores
         and the threshold the user seleects.
-
         Definition:  calculate_gamma_index(self, k):
         Input:       method             - str, name of the method used to compute the outlier score values
                      threshold          - float, all data points having an outlier score > threshold are considered
@@ -188,21 +182,23 @@ class EvaData():
             # Construct pandas data frame with inliers as one column and outliers as the other column
             dataset = pd.DataFrame({'x_1': x_all[:, 0],'x_2': np.zeros(self.n),'Classification': labels})
             fig = px.scatter(dataset, x="x_1", y="x_2", color="Classification",
-                 size=self.outl_scores, title='Outliers and Inliers')
-            return fig
-        if self.d == 2:
-            # Use two dimensions
-            dataset = pd.DataFrame({'x_1': x_all[:, 0], 'x_2': x_all[:, 1], 'Classification': labels})
-            fig = px.scatter(dataset, x="x_1", y="x_2", color="Classification",
-                 size=self.outl_scores, title='Outliers and Inliers')
+                 size=self.outl_scores, title='Outliers amd Inliers')
             return fig
 
-        else:
-            # Use just three dimensions
-            dataset = pd.DataFrame({'x_1': x_all[:, 0], 'x_2': x_all[:, 1], 'x_3': x_all[:, 2], 'Classification': labels})
-            fig = px.scatter_3d(dataset, x="x_1", y="x_2", z="x_3", color="Classification",
-                 size=self.outl_scores, title='Outliers and Inliers')
+        elif self.d == 3:
+            # Construct pandas data for three dimensions
+            dataset = pd.DataFrame({'x_1': x_all[:, 0], 'x_2': x_all[:, 1], 'x_3':x_all[:, 2],'Classification': labels})
+            fig = px.scatter_3d(dataset, x="x_1", y="x_2", z="x_3",color="Classification",
+                 size=self.outl_scores, title='Outliers amd Inliers')
             return fig
+        else:
+            # Use just two dimensions
+            dataset = pd.DataFrame({'x_1': x_all[:, 0], 'x_2': x_all[:, 1], 'Classification': labels})
+            fig = px.scatter(dataset, x="x_1", y="x_2", color="Classification",
+                 size=self.outl_scores, title='Outliers amd Inliers')
+            return fig
+
+
 
     def apply_PCA(self, m):
         ''' Perform PCA analysis on X and reduce to an m dimensional subspace
@@ -210,6 +206,7 @@ class EvaData():
         Input:       X                  - NxD array of N data points with D features
                      m                  - int, dimension of the subspace to project
         '''
+
         self.pca = PCA(n_components=m)
         self.pca.fit(self.X)
         # Update X
@@ -220,12 +217,23 @@ class EvaData():
         # Update n,d
         self.n, self.d = self.X.shape
 
+    def apply_LLE(self, m, neighbours=None, neigh_algo=None):
+        ''' Perform LLE Locally Linear Embedding¶  on X and reduce to an m dimensional subspace
+        Definition:  apply_LLE(X, m)
+        Input:       X                  - NxD array of N data points with D features
+                     m                  - int, dimension of the subspace to project
+        '''
+        embedding = LocallyLinearEmbedding(n_components=m, n_neighbors=neighbours, neighbors_algorithm='kd_tree')
+        # Update X
+        self.X = embedding.fit_transform(self.X)
+        # Update n,d
+        self.n, self.d = self.X.shape
+
     def restore_original_data(self):
         ''' This method restoes X,n,d to the original data
         '''
         self.X = self.X_original
         self.n, self.d = self.X.shape
-
 
 app = dash.Dash()
 app.css.config.serve_locally = False
@@ -245,7 +253,7 @@ app.layout = html.Div(
                             'font-weight': 200,
                             'line-height': 58,
                              'margin': 0,
-                             'backgroundColor':'#DCDCDC'
+                             'backgroundColor': '#DCDCDC'
                               },
                         ),
                 html.Img(
@@ -282,6 +290,9 @@ app.layout = html.Div(
                              options=[
                                  {"label": "Two Gaussians", "value": 'two_gaussians.csv'},
                                  {"label": "Five Gaussians", "value": 'five_gaussians.csv'},
+                                 {"label": "Lab Three Dim", "value": 'lab_data.csv'},
+                                 {"label": "Fishbowl", "value": 'fishbowl.csv'},
+                                 {"label": "Fishbowl Outliers", "value": 'fishbowl_outl.csv'},
                                  {"label": "Iris", "value": 'iris.csv'}],
                              multi=False,
                              value='',  # initial value
@@ -368,12 +379,13 @@ app.layout = html.Div(
                 dcc.Dropdown(id='select red method',
                              options=[
                                  {"label": "PCA", "value": 'pca'},
+                                 {"label": "LLE", "value": 'lle'},
                                  {"label": "TSN", "value": 'tsn'}],
                              multi=False,
                              value='',  # initial value
                              style={'width': '99%'},
                              placeholder='Select dim reduction'
-                             )], className='three columns'),
+                             )], className='two columns'),
 
 
             html.Div([
@@ -386,7 +398,7 @@ app.layout = html.Div(
                     labelPosition='bottom',
                     value=2),
                 html.Div(id='ndim_value')
-            ], className='three columns'),
+            ], className='two columns'),
 
             html.Div([
                 dcc.Dropdown(id='select outlier method reduced',
@@ -416,13 +428,24 @@ app.layout = html.Div(
             html.Div([
                 daq.NumericInput(
                     id='threshold_input_rdim',
-                    min=0.0,
-                    max=1000.0,
-                    size=120.0,
+                    min=0,
+                    max=1000,
+                    size=120,
                     label='threshold value',
                     labelPosition='bottom',
                     value=0),
                 html.Div(id='threshold_value_rdim')
+            ], className='two columns'),
+
+            html.Div([
+                daq.NumericInput(
+                    id='lle_neighbours',
+                    min=0,
+                    max=10000,
+                    size=120,
+                    label='LLE-neighbours',
+                    labelPosition='bottom',
+                    value=5),
             ], className='two columns')
 
         ],
@@ -446,14 +469,15 @@ app.layout = html.Div(
     ], className='eleven columns offset-by-one')
 )
 
-
 # --------------------------------------------------------------
+# Callback to load the dataframe and display it
 # Callback to load the dataframe and display it
 @app.callback(
     [Output(component_id='my_scatter_plot', component_property='figure'),
      Output(component_id='eva_object', component_property='data')],
     [Input(component_id='select dataset', component_property='value')]
 )
+
 def update_graph(selected_dataset):
     if selected_dataset is not '':
         # Init class object
@@ -475,7 +499,7 @@ def update_graph(selected_dataset):
      Input(component_id='outlier detection method', component_property='value'),
      dash.dependencies.Input('threshold_input', 'value')]
 )
-def update_graph_dimred(selected_data_set, k_value, selected_dim_red_method, threshold):
+def update_graph_dimred(selected_data_set, k_value,selected_dim_red_method, threshold):
     # Load the data from a pandas dataframe to a numpy array
     if selected_data_set is '':
         return dash.no_update
@@ -504,21 +528,23 @@ def update_graph_dimred(selected_data_set, k_value, selected_dim_red_method, thr
         else:
             return dash.no_update
 
-
 @app.callback(
     dash.dependencies.Output('k_value', 'children'),
     [dash.dependencies.Input('k_input', 'value')])
+
 def update_output(value):
     pass
-
 
 @app.callback(
     [Output(component_id='red_dim_plot', component_property='figure'),
      Output(component_id='selected_reddim_method', component_property='data')],
     [Input(component_id='select red method', component_property='value'),
      Input(component_id='eva_object', component_property='data'),
-     dash.dependencies.Input('ndim_input', 'value')])
-def update_graph_reddim(selected_redim_method, selected_dataset, m):
+     dash.dependencies.Input('ndim_input', 'value'),
+     dash.dependencies.Input('lle_neighbours', 'value')]
+)
+
+def update_graph_reddim(selected_redim_method, selected_dataset, m, lle_neighbours):
     if selected_redim_method is not '':
         # Init class object
         eva_data = EvaData()
@@ -526,6 +552,9 @@ def update_graph_reddim(selected_redim_method, selected_dataset, m):
         # Apply method
         if selected_redim_method == 'pca':
             eva_data.apply_PCA(m=m)
+
+        elif selected_redim_method == 'lle':
+            eva_data.apply_LLE(m=m, neighbours=lle_neighbours)
         else:
             pass
         # Return scatter figure
@@ -535,7 +564,6 @@ def update_graph_reddim(selected_redim_method, selected_dataset, m):
     else:
         return dash.no_update
 
-
 @app.callback(
     [Output(component_id='outliers_red_dim', component_property='figure')],
     [Input(component_id='selected_reddim_method', component_property='data'),
@@ -543,8 +571,11 @@ def update_graph_reddim(selected_redim_method, selected_dataset, m):
      dash.dependencies.Input('ndim_input', 'value'),
      dash.dependencies.Input('k_input_rdim', 'value'),
      Input(component_id='select outlier method reduced', component_property='value'),
-     dash.dependencies.Input('threshold_input_rdim', 'value')])
-def update_graph_reddim_outliers(selected_redim_method, selected_dataset, m, k, selected_outlier_method, threshold):
+     dash.dependencies.Input('threshold_input_rdim', 'value'),
+     dash.dependencies.Input('lle_neighbours', 'value')]
+)
+
+def update_graph_reddim_outliers(selected_redim_method, selected_dataset, m, k, selected_outlier_method, threshold, lle_neighbours):
     if selected_redim_method is not '':
         if selected_outlier_method is not '':
             # Init class object
@@ -553,6 +584,9 @@ def update_graph_reddim_outliers(selected_redim_method, selected_dataset, m, k, 
             # Apply method
             if selected_redim_method == 'pca':
                 eva_data.apply_PCA(m=m)
+
+            elif selected_redim_method == 'lle':
+                eva_data.apply_LLE(m=m, neighbours=lle_neighbours)
             else:
                 pass
             # Compute outlier score
@@ -576,7 +610,6 @@ def update_graph_reddim_outliers(selected_redim_method, selected_dataset, m, k, 
             return dash.no_update
     else:
         return dash.no_update
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
