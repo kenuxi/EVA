281015d56d075884e51e1c30918ec7166a34e0eb
alrod97 <60501082+alrod97@users.noreply.github.com>
2020-06-09 16:29:29 +0200

    Update Dashboards.py
    new

--- application/plotlydash/Dashboards.py
+++ application/plotlydash/Dashboards.py
@@ -8,7 +8,167 @@ from config import iris_config, external_stylesheets
 from abc import abstractmethod, ABC
 from .assets.layout import html_layout
 
+import numpy as np
+import pandas as pd
+from sklearn.manifold import LocallyLinearEmbedding
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import plotly.express as px
 
+class EvaData():
+    def __init__(self):
+        self.dataset = ''
+        self.pandas_data_frame = None
+        self.X = None
+        self.Y = None
+        self.X_original = None
+        self.outl_score = None
+        self.d = None
+        self.n = None
+        self.labels = False
+        self.labels_name = None
+        self.file_name = None
+        self.feature_names = None
+        self.outl_scores = None
+        self.remained_variance = None
+
+    def load_data(self, file_name):
+        ''' Loads a csv file into a numpy data X and labels y
+        Definition:  load_data(self, file_name)
+        Input:       fname   - string, file name ending in .csv
+        '''
+        self.file_name = file_name
+        # Check data frame is in the correct .csv format
+        if self.file_name.endswith('.csv'):
+            # CSV File with the last columns = labels if there are any labels
+            self.pandas_data_frame = pd.read_csv(self.file_name)
+            X_all = self.pandas_data_frame.to_numpy()
+            self.dataset_columns = self.pandas_data_frame.columns
+            # Extract nxd data matrix and dimensions
+            self.X = X_all[:, :-1].astype('float64')
+            self.X_original = self.X
+            self.n, self.d = self.X.shape
+            self.labels_outlier = self.pandas_data_frame['Classification']
+            # Save variable names
+            self.feature_names = self.pandas_data_frame.keys()
+            # Now we've got the nxd data array X and its corresponding labels Y (if NO labels exist,
+            #  this is indicated by the self.labels bool instance)
+
+        else:
+            print('Error No CSV FILE') # here maybe some dash error widget-message
+
+    def visualize_original_data(self):
+        ''' If the data is 2Dim or 3Dim, it displays the data in a 2D/3D Plot
+        Definition:  visualize_original_data(self)
+        '''
+        if self.d == 1:
+            fig = px.scatter(self.pandas_data_frame, x=self.dataset_columns[0], y=np.zeros(self.n),
+                             color='Classification')
+            return fig
+
+        elif self.d == 2:
+            fig = px.scatter(self.pandas_data_frame, x=self.dataset_columns[0], y=self.dataset_columns[1],
+                             color='Classification')
+            return fig
+        elif self.d == 3:
+            fig = px.scatter_3d(self.pandas_data_frame, x=self.dataset_columns[0], y=self.dataset_columns[1],
+                             z=self.dataset_columns[2], color='Classification')
+            return fig
+        else:
+            # If data is more then three dim just plot the first 2 dimensions
+            fig = px.scatter(self.pandas_data_frame, x=self.dataset_columns[0], y=self.dataset_columns[1],
+                             color='Classification')
+            return fig
+
+
+    def visualize_reduced_data(self):
+        ''' If the data is 1DIM, 2Dim or 3Dim, it displays the data in a 1D/2D/3D Plot
+        Definition:  visualize_reduced_data(self)
+        '''
+        # read columns
+        columns_data = self.reduced_pandas_data.columns
+        if self.d_red == 1:
+            fig = px.scatter(self.reduced_pandas_data, x=columns_data[0], y=np.zeros(self.n),
+                             color='Classification')
+            return fig
+
+        elif self.d_red == 2:
+            fig = px.scatter(self.reduced_pandas_data, x=columns_data[0], y=columns_data[1],
+                             color='Classification')
+            return fig
+        elif self.d_red == 3:
+            fig = px.scatter_3d(self.reduced_pandas_data, x=columns_data[0], y=columns_data[1],
+                             z=columns_data[2], color='Classification')
+            return fig
+        else:
+            # If data is more then three dim just plot the first 2 dimensions
+            fig = px.scatter(self.reduced_pandas_data, x=columns_data[0], y=columns_data[1],
+                             color='Classification')
+            return fig
+
+    def apply_PCA(self, m):
+        ''' Perform PCA analysis on X and reduce to an m dimensional subspace
+        Definition:  apply_pca(X, m)
+        Input:       X                  - NxD array of N data points with D features
+                     m                  - int, dimension of the subspace to project
+        '''
+
+        self.pca = PCA(n_components=m)
+        self.pca.fit(self.X)
+        # Update X
+        self.X_red = self.pca.transform(self.X)
+        variance_components = self.pca.explained_variance_ratio_
+        self.remained_variance = np.sum(variance_components[:m]) * 100
+
+        # Update n,d
+        self.d_red = m
+
+        # Build pandas dataframe with PCA reduced data
+        self.reduced_pandas_data = pd.DataFrame(data=self.X_red)
+        self.reduced_pandas_data['Classification'] = self.pandas_data_frame['Classification']
+        self.red_method = 'PCA'
+
+    def apply_LLE(self, m, neighbours=5, neigh_algo=None):
+        ''' Perform LLE Locally Linear Embedding¶  on X and reduce to an m dimensional subspace
+        Definition:  apply_LLE(X, m)
+        Input:       X                  - NxD array of N data points with D features
+                     m                  - int, dimension of the subspace to project
+        '''
+        embedding = LocallyLinearEmbedding(n_components=m, n_neighbors=neighbours, neighbors_algorithm='kd_tree')
+        # Update X
+        self.X_red = embedding.fit_transform(self.X)
+        # Update n,d
+        self.d_red = m
+        # Build pandas dataframe with LLE reduced data
+        self.reduced_pandas_data = pd.DataFrame(data=self.X_red)
+        self.reduced_pandas_data['Classification'] = self.pandas_data_frame['Classification']
+        self.red_method = 'LLE'
+
+    def apply_TSNE(self, m, perplexity=30):
+        ''' Perform t-distributed Stochastic Neighbor Embedding. on X and reduce to an m dimensional subspace
+        Definition:  apply_LLE(X, m)
+        Input:       X                          - NxD array of N data points with D features
+                     m                          - int, dimension of the subspace to project
+                     perplexity                 - float, related to the number of nearest neighbors that is used in other
+                                                  manifold learning algorithms. Larger datasets usually require a larger
+                                                  perplexity. Consider selecting a value between 5 and 50. Different
+                                                  values can result in significanlty different results.
+        '''
+        embedding = TSNE(n_components=m, perplexity=perplexity)
+        # Update X
+        self.X_red = embedding.fit_transform(self.X)
+        # Update n,d
+        self.d_red = m
+        # Build pandas dataframe with LLE reduced data
+        self.reduced_pandas_data = pd.DataFrame(data=self.X_red)
+        self.reduced_pandas_data['Classification'] = self.pandas_data_frame['Classification']
+        self.red_method = 'TSNE'
+
+    def restore_original_data(self):
+        ''' This method restoes X,n,d to the original data
+        '''
+        self.X = self.X_original
+        self.n, self.d = self.X.shape
 class Dashboard(ABC):
     def __init__(self, server: flask.Flask, stylesheets: list, prefix='/dash'):
         self.server = server
@@ -55,45 +215,26 @@ class IrisDashboard(RemoteCSVDashboard):
                                   routes_pathname_prefix=self.prefix,
                                   external_stylesheets=self.stylesheets)
 
-    def init_callbacks(self, target_column):
-        @self.dash_app.callback(
-            Output('iris_graph', 'figure'),
-            [Input('feat_1', 'value'),
-             Input('feat_2', 'value')]
-        )
-        def update_graph(feat_1, feat_2):
-            return {'data': [
-                dict(
-                    x=self.df[self.df[target_column] == name][feat_1],
-                    y=self.df[self.df[target_column] == name][feat_2],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=name
-                ) for name in self.df[target_column].unique()
-            ],
-                'layout': dict(
-                    xaxis={'type': 'Linear', 'title': feat_1},
-                    yaxis={'title': feat_2},
-                    margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                    legend={'x': 0, 'y': 1},
-                    hovermode='closest',
-                )}
 
-    def create_dashboard(self, target_column=iris_config['target']):
-        features = iris_config['features']  # Hardcoded
+    def create_dashboard(self, data_dict):
+
+        data_file_name = data_dict['location']# '/Users/albertorodriguez/Desktop/Current Courses/InternenServLab/EVA-merge_flask_dash/application/data/fishbowl_outl.csv'
+        dim_red_method = data_dict['target']
+
+        main_data = EvaData()
+        main_data.load_data(file_name=data_file_name)
+
+        if dim_red_method == 'pca':
+            main_data.apply_PCA(m=2)
+
+        fig_original = main_data.visualize_original_data()
+        fig_reduced = main_data.visualize_reduced_data()
 
         self.dash_app.index_string = html_layout
 
         self.dash_app.layout = html.Div(children=[
-            html.Div([iris_feature_selector(features[0], features, 'feat_1')],
-                     style={'width': '48%', 'display': 'inline-block'}),
-            html.Div([iris_feature_selector(features[1], features, 'feat_2')],
-                     style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
-            dcc.Graph(id='iris_graph')
+            dcc.Graph(id='outliers_red_dim', figure=fig_original),
+            dcc.Graph(id='my_scatter_plot', figure=fig_reduced)
         ])
-        self.init_callbacks(target_column)
+
         return self.dash_app.server
